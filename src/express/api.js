95ca979954f4d2479e8a794f3a8c6966970eb561
Tristan Vermeesch <titivermeesch@gmail.com>
2021-05-21 22:30:45 +0200

    remove old way of authenticating

--- src/express/api.js
+++ src/express/api.js
@@ -1,26 +1,25 @@
 import axios from 'axios'
 import express from 'express'
 import qs from 'qs'
+import { User } from '../models/User.js'
 
 const app = express()
 
-const clientId = 'c56d4108a56d4b71be19528c8732301f'
-const clientSecret = '12b3a73ffab743d084e752e82cceb3a7'
+const clientId = process.env.SPOTIFY_CLIENT
 app.get('/login/:discordId', (req, res) => {
   const url = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=http://localhost:3000/success&scope=user-read-playback-state,user-modify-playback-state,user-read-currently-playing&state=${req.params.discordId}`
   res.redirect(url)
 })
 
 app.get('/success', async (req, res) => {
-  console.log(req.query)
-  res.status(200).send('Logged in')
+  res.status(200).send('Thanks for logging in. You can close this page')
 
   const postRes = await axios({
     method: 'POST',
     url: 'https://accounts.spotify.com/api/token',
     data: qs.stringify({
       client_id: clientId,
-      client_secret: clientSecret,
+      client_secret: process.env.SPOTIFY_SECRET,
       grant_type: 'authorization_code',
       code: req.query.code,
       redirect_uri: 'http://localhost:3000/success',
@@ -30,7 +29,11 @@ app.get('/success', async (req, res) => {
     },
   })
 
-  console.log(postRes)
+  await User.create({
+    discordUserId: req.query.state,
+    accessToken: postRes.data.access_token,
+    refreshToken: postRes.data.refresh_token,
+  })
 })
 
 app.listen(3000, () => console.log('Express backend running on :3000'))
